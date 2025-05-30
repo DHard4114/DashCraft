819b07109ee4cb465eb463ec989c8f582f873ae8
Daffa Hardhan <dapahardan@gmail.com>
2025-05-30 19:59:33 +0700

    Progress 95%

--- BE/middleware/corsMiddleware.js
+++ BE/middleware/corsMiddleware.js
@@ -1,7 +1,7 @@
 const cors = require('cors');
 
 const corsOptions = {
-    origin: ['http://localhost:3000','http://localhost:5173', process.env.VITE_API],
+    origin: ['http://localhost:3000','http://localhost:5173', process.env.FRONTEND_URL],
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
 };
 
